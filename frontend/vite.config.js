407a19c092d098bde191d997cbcc58c312f7f37e
APPLE <apple@APPLEs-MacBook-Air-584.local>
2026-05-12 17:17:35 +0530

    fixing target link

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://smart-expense-tracker-production-60e4.up.railway.app',
         changeOrigin: true,
       },
     },
